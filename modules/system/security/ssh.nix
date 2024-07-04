f0b585d85f5e71e48997a9fc85200085923ce904
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2024-07-04 20:27:01 +0200

    Refactor path for SSH host key in system configuration

--- modules/system/security/ssh.nix
+++ modules/system/security/ssh.nix
@@ -33,7 +33,7 @@ in
 
       hostKeys = [
         {
-          path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
+          path = "${lib.optionalString hasOptinPersistence "/persist/system"}/etc/ssh/ssh_host_ed25519_key";
           type = "ed25519";
         }
       ];
