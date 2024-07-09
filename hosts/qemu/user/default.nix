4cac340760509ed61df2b9358f99fda9f758ffe9
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2024-07-09 15:46:35 +0200

    Add monitor configuration for Hyprland window manager

--- hosts/qemu/user/default.nix
+++ hosts/qemu/user/default.nix
@@ -16,4 +16,13 @@
   #   WLR_RENDERER_ALLOW_SOFTWARE = 1;
   #   WLR_NO_HARDWARE_CURSORS = 1;
   # };
+  modules.user.wm.monitors = [
+    {
+      name = "Virtual-1";
+      primary = true;
+      width = 1920;
+      height = 1080;
+      workspace = "1";
+    }
+  ];
 }
--- modules/user/wm/default.nix
+++ modules/user/wm/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./hyprland.nix
     ./waybar.nix
+    ./monitors.nix
   ];
 }
