90fbfd985048e14610a4e5eac87c70fa8f03b258
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2024-08-19 17:59:01 +0200

    Switch from SDDM to greetd

--- profiles/default/system/configuration.nix
+++ profiles/default/system/configuration.nix
@@ -28,10 +28,10 @@
   modules.system.wm.hyprland.enable = true;
 
   # Enable the greetd login manager.
-  # modules.system.desktop.greetd.enable = true;
+  modules.system.desktop.greetd.enable = true;
 
   # Enable the SDDM login manager.
-  modules.system.desktop.sddm.enable = true;
+  # modules.system.desktop.sddm.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
